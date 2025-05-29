474c364719340b8cd4adb2bca9a45d6c60e8d698
Aya-darwish <aya.darwish.ad004@gmail.com>
2025-05-29 12:08:34 +0100

    I do it to add welcome and about us

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import HeaderMenu from './Header.js';
+import Welcome from './Welcome';
+import AboutUs from './AboutUs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderMenu />
+      <Welcome />
+      <AboutUs />
     </div>
   );
 }
